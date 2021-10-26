d9972f7943b3848215a02ca3a9ec16554524a0e9
Valentin Purrucker <vali.pur13@gmail.com>
2021-10-26 14:24:23 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Valentin Purrucker on 26.10.21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
